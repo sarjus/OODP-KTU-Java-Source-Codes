f78cc12bcc46cc9484411e9c876022745da50f1c
Sarju S <sarju.s@sjcetpalai.ac.in>
2019-02-05 05:11:18 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+	 /* Author: Sarju S
+		Date: 05-01-2018
+	  	  Description: Writes the words "Hello Java" on the screen */
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+
+	}
+
+}
