8d529f8d113b066a7548cb1bf511fb73e1c2a39c
Sarju S <sarju.s@sjcetpalai.ac.in>
2022-01-20 19:01:22 +0530

    Add files via upload

---
+++ arithmetic/Demo.java
@@ -0,0 +1,12 @@
+package com.sjcet.arithmetic;
+//importing a package
+import com.sjcet.calculator.MyCalculator;
+public class Demo {
+	public static void main(String args[]){
+			MyCalculator obj = new MyCalculator();
+			System.out.println(obj.add(100, 200));
+		   }
+
+	
+
+}
